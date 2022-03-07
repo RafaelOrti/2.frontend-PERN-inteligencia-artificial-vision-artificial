9893b872225dbcc14ebdddfa3b36446958c845ae
RafaOrti <rafaeloriolorticarrio@gmail.com>
2022-03-08 00:38:20 +0100

    Define log out

--- app-ia-peliculas/src/Components/Header/Header.jsx
+++ app-ia-peliculas/src/Components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
+import { LOGOUT } from '../../redux/types';
+import {connect} from 'react-redux';
 
 import './Header.css';
 
-const Header = () => {
+const Header = (props) => {
 
     let navigate = useNavigate();
 
@@ -15,16 +17,38 @@ const Header = () => {
 
     }
 
+    const logOut = () => {
+        //Borrar de RDX las credenciales
+        props.dispatch({type:LOGOUT});
+
+        setTimeout(()=>{
+            navigate("/");
+        },1500);
+    }
+    
+    if(!props?.credentials?.token){
+        return (
+            <div className='designHeader'>
+                <div className="headerSpace"></div>
+                <div className="headerSpace"></div>
+                <div className="headerSpace linksDesign">
+                    <div className="link" onClick={()=>navegar("/login")}>Login</div>
+                    <div className="link" onClick={()=>navegar("/register")}>Registro</div>    
+                </div>
+            </div>
+        )
+    }else {
     return (
         <div className='designHeader'>
-            <div className="headerSpace"></div>
-            <div className="headerSpace"></div>
-            <div className="headerSpace linksDesign">
-                <div className="link" onClick={()=>navegar("/login")}>Login</div>
-                <div className="link" onClick={()=>navegar("/register")}>Registro</div>    
+                <div className="headerSpace"></div>
+                <div className="headerSpace"></div>
+                <div className="headerSpace linksDesign">
+                    <div className="link" onClick={()=>navegar("/profile")}>{props.credentials?.usuario.nombre}</div>
+                    <div className="link" onClick={()=>logOut()}>Logout</div>    
             </div>
         </div>
     )
+    }
 
 }
 
