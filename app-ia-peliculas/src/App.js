22d5b5ad53ebf6e57ee628e3bfd26bf0825febf9
RafaOrti <rafaeloriolorticarrio@gmail.com>
2022-03-07 18:52:08 +0100

    Define structure of Home
    header, footer and Routes

--- app-ia-peliculas/src/App.js
+++ app-ia-peliculas/src/App.js
@@ -2,10 +2,34 @@
 import './App.css';
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Containers/Home/Home';
+import Login from './Containers/Login/Login';
+
 
 function App() {
   return (
     <div className="App">
+
+      <BrowserRouter>
+      
+        <Header/>
+
+        <Routes>
+
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        
+
+
+        </Routes>
+          
+        
+
+        <Footer/> 
+      
+      </BrowserRouter>
       
     </div>
   );
---
+++ app-ia-peliculas/src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            soy Footer
+        </div>
+    )
+}
+
+export default Footer;
