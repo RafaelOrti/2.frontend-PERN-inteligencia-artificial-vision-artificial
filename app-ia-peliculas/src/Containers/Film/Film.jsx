6c8ea3c76d59e33122b0059edb6e5317e02e9c75
RafaOrti <rafaeloriolorticarrio@gmail.com>
2022-03-18 22:41:22 +0100

    feat: create films av searcher

--- app-ia-peliculas/src/Containers/Film/Film.jsx
+++ app-ia-peliculas/src/Containers/Film/Film.jsx
@@ -24,62 +24,191 @@ const Film = (props) => {
     console.log("pagina cargada", props.emotions?.timer)
 
     const [films, setFilms] = useState([]);
+    const [filmsTerror, setFilmsTerror] = useState([]);
+    const [filmsAccion, setFilmsAccion] = useState([]);
+    const [filmsHumor, setFilmsHumor] = useState([]);
+    const [filmsAI, setFilmsAI] = useState("");
+    const [filmsAV, setFilmsAV] = useState("");
     // let [av, setAv] = useState(0);
     let navigate = useNavigate();
-
+    let body;
     let i = 0;
 
-
+    //elementos cargados al inicio
     useEffect(() => {
         console.log('Created')
         props.dispatch({ type: NOT_HOME })//grande Luigi!!!
         traePelis();
+        traePelisTerror();
+        traePelisHumor();
+        traePelisAccion();
+        traePelisAI();
     }, [])
 
-    const uploadResults = (emotion) => {
-        console.log(emotion, props.credentials?.usuario.id)
-    }
-
-    // useEffect(() => {
-    //     a++;
-    //     if(a===2000){
-    //         a=0;
-            
-    //     }
-    // }, [a])
-
-    setTimeout(() => {
-        a++;
-        console.log(a);
-        if(a===20){
-            a=0;
-            uploadResults(props.emotions?.emotion);
-        }
-      }, 500)
-
+    //llamada directa a backend
     const traePelis = async () => {
         try {
             let res = await axios.get(raiz + "peliculas/", { headers: { "Authorization": `Bearer ${props.credentials?.token}` } });
-            console.log("res")
-            console.log(res)
+            // console.log("res")
+            // console.log(res)
             setTimeout(() => {
-                console.log("res2")
-                console.log(res.data)
+                // console.log("res2")
+                // console.log(res.data)
                 setFilms(res.data);
             }, 2);
         } catch (error) {
             console.log(error);
         }
     };
-    const traePelisAV = async () => {
+
+
+    const traePelisTerror = async () => {
         try {
-            let res = await axios.post(raiz + "usuarios/dataset", { headers: { "Authorization": `Bearer ${props.credentials?.token}` } });
-            console.log("res")
+            body = {
+                genero: "terror"
+            }
+            console.log("reseee")
+            let res = await axios.post(raiz + "peliculas/genero", body);
+            console.log("reseee")
             console.log(res)
+            setTimeout(() => {
+                // console.log("res2")
+                // console.log(res.data)
+                setFilmsTerror(res.data);
+            }, 2);
         } catch (error) {
             console.log(error);
         }
     };
+
+    const traePelisHumor = async () => {
+        try {
+            let res = await axios.post(raiz + "peliculas/genero", {
+                genero: "humor"
+            });
+            // console.log("res")
+            // console.log(res)
+            setTimeout(() => {
+                // console.log("res2")
+                // console.log(res.data)
+                setFilmsHumor(res.data);
+            }, 2);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+    const traePelisAccion = async () => {
+        try {
+            let res = await axios.post(raiz + "peliculas/genero", {
+                genero: "accion"
+            });
+            // console.log("res")
+            // console.log(res)
+            setTimeout(() => {
+                // console.log("res2")
+                // console.log(res.data)
+                setFilmsAccion(res.data);
+            }, 2);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+
+
+
+    //llamada con procesado red neuronal de recomendaciones
+    const traePelisAI = async () => {
+        try {
+
+            //http://localhost:3000/peliculas
+            let res = await axios.post(raiz + `peliculas/ia/${props.credentials?.usuario.id}`, { headers: { "Authorization": `Bearer ${props.credentials?.token}` } });
+
+            // let config = {
+            //     headers: { Authorization: Bearer ${props.credenciales.token} }
+            // };
+            // let res = await axios.post("https://rgd-videoclub-backend.herokuapp.com/pedidos/admin", body, config);
+            // console.log("resultado AI")
+            // console.log(res)
+            setTimeout(() => {
+                // console.log("res2")
+                // console.log(res.data)
+                setFilmsAI(res.data);
+            }, 2);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+
+    //elementos cargados cada x segundos
+
+
+    setTimeout(() => {
+        a++;
+        console.log(a);
+        if (a === 10) {
+            a = 0;
+            traePelisAV(props.emotions?.emotion);
+        }
+    }, 500)
+
+
+    //Procesado de datos cad x segundos a traves de redux para hacer llamada a backend
+
+    const traePelisAV = async (emotion) => {
+        try {
+            body = {
+                angry: emotion.angry,
+                disgusted: emotion.disgusted,
+                fearful: emotion.fearful,
+                happy: emotion.happy,
+                neutral: emotion.neutral,
+                sad: emotion.sad,
+                surprised: emotion.surprised,
+                id: props.credentials?.usuario.id
+            }
+            console.log("res222")
+            console.log(body)
+
+            let res = await axios.post(raiz + `peliculas/ias`, body);
+            setTimeout(() => {
+                // console.log("res2")
+                // console.log(res.data)
+                setFilmsAV(res.data);
+            }, 2);
+
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+
+
+
+
+
+    // useEffect(() => {
+    //     a++;
+    //     if(a===2000){
+    //         a=0;
+
+    //     }
+    // }, [a])
+
+    // const uploadResults = (emotion) => {
+    //     console.log(emotion, props.credentials?.usuario.id)
+    //     console.log("emotion, props.credentials?.usuario.id")
+    //     console.log(props.credentials?.usuario.id)
+    // }
+
+    //redirecion a movie detail
 
     const escogePelicula = (pelicula) => {
         //Guardamos la pelicula escogida en redux
@@ -88,7 +217,10 @@ const Film = (props) => {
         navigate("/moviedetail");
     }
 
-    if (films[0]?.id !== undefined) {
+
+
+
+    if (films[0]?.id !== undefined && filmsTerror[0]?.id !== undefined && filmsHumor[0]?.id !== undefined && filmsAccion[0]?.id !== undefined && filmsAI[0]?.id !== undefined) {
         return (
             <div className='designFilm'>
                 <br />
@@ -98,22 +230,132 @@ const Film = (props) => {
                 <br />
                 <Camera mode={false} />
                 <div className='designFilmSubFilm'>
+                    <p>Peliculas AV AI</p>
+                    <div className="container">
+                        {
+
+                            (filmsAV !== "") &&
+                            (
+                                filmsAV.slice(i, i + 10).map(peliculaAV => {
+                                    // console.log("imagen")
+                                    // console.log(peliculaAV.imagen)
+                                    return (
+                                        <div className="item" key={peliculaAV.id} onClick={() => escogePelicula(peliculaAV)}>
+                                            <img className="fotoCard" src={peliculaAV.imagen} alt={peliculaAV.titulo} />
+                                            <p className="fotoName">{peliculaAV.titulo}</p>
+                                        </div>
+                                    )
+                                }))
+                        }
+                        {
+                            console.log(filmsAV)
+                        }
+                        {
+
+
+                            (filmsAV === "") &&
+                            (
+                                films.slice(i, i + 10).map(pelicula => {
+                                    return (
+                                        <div className="item" key={pelicula.id} onClick={() => escogePelicula(pelicula)}>
+                                            <img className="fotoCard" src={pelicula.imagen} alt={pelicula.titulo} />
+                                            <p className="fotoName">{pelicula.titulo}</p>
+                                        </div>
+                                    )
+                                }))
+                        }
+                    </div>
+                    <p>Peliculas AI</p>
+                    <div className="container">
+                        {
+                            (filmsAI !== "") &&
+                            (
+                                filmsAI.slice(i, i + 10).map(peliculaAI => {
+                                    // console.log("imagen")
+                                    // console.log(peliculaAI.imagen)
+                                    return (
+                                        <div className="item" key={peliculaAI.id} onClick={() => escogePelicula(peliculaAI)}>
+                                            <img className="fotoCard" src={peliculaAI.imagen} alt={peliculaAI.titulo} />
+                                            <p className="fotoName">{peliculaAI.titulo}</p>
+                                        </div>
+                                    )
+                                }))
+                        }
+                        {
+                            (filmsAI === "") &&
+                            (
+                                films.slice(i, i + 10).map(pelicula => {
+                                    return (
+                                        <div className="item" key={pelicula.id} onClick={() => escogePelicula(pelicula)}>
+                                            <img className="fotoCard" src={pelicula.imagen} alt={pelicula.titulo} />
+                                            <p className="fotoName">{pelicula.titulo}</p>
+                                        </div>
+                                    )
+                                }))
+                        }
+                    </div>
+                    <p>Todas las películas</p>
                     <div className="container">
                         {
                             films.slice(i, i + 10).map(pelicula => {
                                 return (
                                     <div className="item" key={pelicula.id} onClick={() => escogePelicula(pelicula)}>
-                                        <img className="fotoCard" src={pelicula.imagen} alt={pelicula.title} />
+                                        <img className="fotoCard" src={pelicula.imagen} alt={pelicula.titulo} />
                                         <p className="fotoName">{pelicula.titulo}</p>
                                     </div>
                                 )
                             })
                         }
-                        {props.emotions?.timer && 
-                        <>
-                        </>
-                        }
+                        {/* {props.emotions?.timer &&
+                            <>
+                            </>
+                        } */}
                     </div>
+                    <p>Películas de terror</p>
+                    <div className="container">
+                        {
+                            filmsTerror.slice(i, i + 10).map(peliculaTerror => {
+                                return (
+                                    <div className="item" key={peliculaTerror.id} onClick={() => escogePelicula(peliculaTerror)}>
+                                        <img className="fotoCard" src={peliculaTerror.imagen} alt={peliculaTerror.titulo} />
+                                        <p className="fotoName">{peliculaTerror.titulo}</p>
+                                    </div>
+                                )
+                            })
+                        }
+
+                    </div>
+                    <p>Películas de humor</p>
+                    <div className="container">
+                        {
+                            filmsHumor.slice(i, i + 10).map(peliculaHumor => {
+                                return (
+                                    <div className="item" key={peliculaHumor.id} onClick={() => escogePelicula(peliculaHumor)}>
+                                        <img className="fotoCard" src={peliculaHumor.imagen} alt={peliculaHumor.titulo} />
+                                        <p className="fotoName">{peliculaHumor.titulo}</p>
+                                    </div>
+                                )
+                            })
+                        }
+
+                    </div>
+                    <p>Películas de accion</p>
+                    <div className="container">
+                        {
+                            filmsAccion.slice(i, i + 10).map(peliculaAccion => {
+                                return (
+                                    <div className="item" key={peliculaAccion.id} onClick={() => escogePelicula(peliculaAccion)}>
+                                        <img className="fotoCard" src={peliculaAccion.imagen} alt={peliculaAccion.titulo} />
+                                        <p className="fotoName">{peliculaAccion.titulo}</p>
+                                    </div>
+                                )
+                            })
+                        }
+
+                    </div>
+
+
+
                 </div>
             </div>
         )
